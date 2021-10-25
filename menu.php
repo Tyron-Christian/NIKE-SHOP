7457ef2378c97747d9e47a714dbc7e50804452be
Tyron-Christian <chriistudio@gmail.com>
2021-10-25 23:03:37 +0200

    VERSIONE 1.0
    Settato il menù lato html e css.
    
    -Aggiungere funzione javascript del menù.

---
+++ menu.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/fontawesome.min.css">
+    <link rel="stylesheet" href="style.css">
+    
+    <title>NIKE SPORT SHOP</title>
+</head>
+<body>
+
+
+<div class="menu">
+  <div class="titolo">
+  <p class="logo">NIKE SHOP</p>
+  </div>
+
+    <div class="elementi">
+        <p>Nuovi arrivi</p>
+        <p>Uomo</p>
+        <p>Donna</p>
+        <p>Collezioni</p>
+    </div>
+    <p class="hamburger"><i class="fas fa-bars"></i></p>
+</div>    
+       
+
+
+    <script src="script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+</body>
+</html>
